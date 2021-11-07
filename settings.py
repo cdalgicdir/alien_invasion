fc932551648a8f1a8a04f354a78cafd9039acc66
Cahit Dalgicdir <cdalgicdir@ku.edu.tr>
2021-11-07 13:41:29 +0100

    shoot aliens

--- settings.py
+++ settings.py
@@ -5,12 +5,18 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
         self.ship_speed = 1.5
 
+        # Bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
-    
+        # Alien settings
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 5
+        self.fleet_direction = 1 # 1: right, -1: left
